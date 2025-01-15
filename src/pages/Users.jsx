96207aed961b5119fbaa32324d764ca5aa6f9169
bieucbt <bieucbt@gmail.com>
2025-01-15 16:29:57 +0700

    update

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -19,8 +19,9 @@ const Users = () => {
 
   return (
     <div className=''>
-      <div className='grid grid-cols-5 place-items-center mb-4'>
-        <div>STT</div>
+      <div className='grid grid-cols-6 place-items-center mb-4'
+      style={{gridTemplateColumns: 'auto repeat(5, 1fr)'}}>
+        <div className='w-[40px] px-1 text-center'>STT</div>
         <div>tên người dùng</div>
         <div>SĐT</div>
         <div>tài khoản</div>
@@ -30,12 +31,13 @@ const Users = () => {
       {
         userData.map((item, i) => (
         <div key={i}
-        className='grid grid-cols-5 place-items-center mb-[6px]'>
-          <div>{i}</div>
-          <div>{item.lastName + ' ' +item.firstName || 'name user'}</div>
+        className='grid place-items-center mb-[6px] gap-2'
+        style={{gridTemplateColumns: 'auto repeat(5, 1fr)'}}>
+          <div className='w-[40px] px-1 text-center'>{i}</div>
+          <div className='w-full overflow-hidden text-ellipsis'>{item.lastName + ' ' +item.firstName || 'name user'}</div>
           <div>{item.phone}</div>
           <div>{item.email}</div>
-          <div className='overflow-hidden max-w-[200px]'><span className='line-clamp-2 text-ellipsis'>{item.password}</span></div>
+          <div className='w-full text-ellipsis text-wrap overflow-hidden'>{item.password}</div>
           <div className='flex items-center gap-3'>
             <div>sửa</div>
             <div className='cursor-pointer' onClick={() => handleDeleteUser(item._id)}>xóa</div>
