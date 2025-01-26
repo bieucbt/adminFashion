3a107b8f5dac39029b52f98f070abf6acd8f1257
bieucbt <bieucbt@gmail.com>
2025-01-26 15:27:17 +0700

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,10 +1,10 @@
 
 
-// export const BASE_URL_USER = 'https://servertest-chi.vercel.app/user/';
-// export const BASE_URL_PRODUCT = 'https://servertest-chi.vercel.app/product/';
+export const BASE_URL_USER = 'https://servertest-chi.vercel.app/user/';
+export const BASE_URL_PRODUCT = 'https://servertest-chi.vercel.app/product/';
 
-const BASE_URL_USER = 'http://localhost:3000/user/';
-const BASE_URL_PRODUCT = 'http://localhost:3000/product/';
+// const BASE_URL_USER = 'http://localhost:3000/user/';
+// const BASE_URL_PRODUCT = 'http://localhost:3000/product/';
 const CLOUDINARY_URL = 'https://res.cloudinary.com/ddttnwsqh/image/upload/v1737878119/'
 export {
     BASE_URL_PRODUCT,
