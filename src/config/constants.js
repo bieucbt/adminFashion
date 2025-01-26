602b89f4367e985b22044e47a412e421a453c176
bieucbt <bieucbt@gmail.com>
2025-01-26 15:38:30 +0700

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,6 +3,7 @@
 export const BASE_URL_USER = 'https://servertest-chi.vercel.app/user/';
 export const BASE_URL_PRODUCT = 'https://servertest-chi.vercel.app/product/';
 
+
 // const BASE_URL_USER = 'http://localhost:3000/user/';
 // const BASE_URL_PRODUCT = 'http://localhost:3000/product/';
 const CLOUDINARY_URL = 'https://res.cloudinary.com/ddttnwsqh/image/upload/v1737878119/'
