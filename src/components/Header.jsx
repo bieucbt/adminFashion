3d81793a7a984ef003e3c3c1632d8ea862503127
bieucbt <bieucbt@gmail.com>
2025-02-03 15:49:07 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { memo, useEffect, useRef } from 'react'
+import { HOME_URL } from '../config/constants';
 
-const Header = ({headerHeight, setHeaderHeight}) => {
+const Header = ({ headerHeight, setHeaderHeight }) => {
   const headerRef = useRef(null)
 
   useEffect(() => {
@@ -10,7 +11,7 @@ const Header = ({headerHeight, setHeaderHeight}) => {
     <header ref={headerRef} className='fixed top-0 left-0 right-0 border-b-gray-200 border-[2px] bg-white
     z-50 '>
       <div className='container mx-auto px-4 flex items-center justify-between h-full py-3'>
-        <div>
+        <div onClick={() => window.location.href = HOME_URL} >
           <div className='brand'>BieuCBT.</div>
           <div>Admin Panel</div>
         </div>
--- src/config/constants.js
+++ src/config/constants.js
@@ -2,14 +2,17 @@
 
 // const BASE_URL_USER = 'https://servertest-mmh4.onrender.com/user/';
 // const BASE_URL_PRODUCT = 'https://servertest-mmh4.onrender.com/product/';
+// const HOME_URL = 'https://fashion-ewea.onrender.com'
 
 
 const BASE_URL_USER = 'http://localhost:3000/user/';
 const BASE_URL_PRODUCT = 'http://localhost:3000/product/';
+const HOME_URL = 'http://localhost:5173/'
 
 const CLOUDINARY_URL = 'https://res.cloudinary.com/ddttnwsqh/image/upload/v1737878119/'
 export {
     BASE_URL_PRODUCT,
     BASE_URL_USER,
-    CLOUDINARY_URL
+    CLOUDINARY_URL,
+    HOME_URL
 }
