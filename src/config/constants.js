96def1aafdd118fad412b64b20020f20852eefa6
bieucbt <bieucbt@gmail.com>
2025-02-05 16:13:10 +0700

    test

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,13 +1,13 @@
 
 
-// const USER_URL = 'https://servertest-mmh4.onrender.com/user/';
-// const PRODUCT_URL = 'https://servertest-mmh4.onrender.com/product/';
-// const HOME_URL = 'https://fashion-ewea.onrender.com'
+const USER_URL = 'https://servertest-mmh4.onrender.com/user/';
+const PRODUCT_URL = 'https://servertest-mmh4.onrender.com/product/';
+const HOME_URL = 'https://fashion-ewea.onrender.com'
 
 
-const USER_URL = 'http://localhost:3000/user/';
-const PRODUCT_URL = 'http://localhost:3000/product/';
-const HOME_URL = 'http://localhost:5173/'
+// const USER_URL = 'http://localhost:3000/user/';
+// const PRODUCT_URL = 'http://localhost:3000/product/';
+// const HOME_URL = 'http://localhost:5173/'
 
 const CLOUDINARY_URL = 'https://res.cloudinary.com/ddttnwsqh/image/upload/v1737878119/'
 export {
